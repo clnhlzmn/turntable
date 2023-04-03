04da3744f4c2fb94296332d8bcfa83b0b5bdb521
Colin <user@email.com>
2023-04-03 04:30:27 +0100

    added shutter pin

--- turntable/__main__.py
+++ turntable/__main__.py
@@ -1,10 +1,57 @@
 import time
-from adafruit_motorkit import MotorKit
+import sys
+
+import RPi.GPIO as GPIO
 from adafruit_motor import stepper
+from adafruit_motorkit import MotorKit
 
-kit = MotorKit()
+# BCM pin number for shutter open drain
+SHUTTER_PIN = 17
 
-for _ in range(3200):
-    kit.stepper1.onestep(style=stepper.MICROSTEP)
+# stepper motor steps per full rotation
+MOTOR_STEPS = 200
 
-kit.stepper1.release()
+class Turntable:
+    def __init__(self, shutter_pin, motor_steps):
+        n_microsteps = 16
+        self.kit = MotorKit(steppers_microsteps=n_microsteps)
+        self.steps_per_rotation = n_microsteps * motor_steps
+        self.shutter_pin = shutter_pin
+
+
+    def __enter__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.shutter_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        return self
+
+
+    def __exit__(self, *_):
+        self.kit.stepper1.release()
+
+
+    def shutter(self):
+        GPIO.setup(self.shutter_pin, GPIO.OUT, initial=GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.setup(self.shutter_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+    def turn(self, angle):
+        for _ in range(int(angle / 360.0 * self.steps_per_rotation)):
+            self.kit.stepper1.onestep(style=stepper.MICROSTEP)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print('python -m turntable <step angle (degs)> <delay (s)>')
+        exit(-1)
+
+    angle = float(sys.argv[1])
+    delay = float(sys.argv[2])
+
+    with Turntable(SHUTTER_PIN, MOTOR_STEPS) as tt:
+        total_angle = 0.0
+        while total_angle < 360.0:
+            tt.shutter()
+            time.sleep(delay)
+            tt.turn(angle)
+            total_angle += angle
